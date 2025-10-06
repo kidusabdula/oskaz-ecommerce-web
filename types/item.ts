15b1d6f5bbee63c19784747a855488081a73b02a
Kidus Abdula <Kidus489@gmail.com>
2025-10-06 16:02:35 +0300

    Item Type List

---
+++ types/item.ts
@@ -0,0 +1,35 @@
+export interface Item {
+    name: string;
+    item_code: string;
+    item_name: string;
+    stock_uom?: string;
+    description?: string;
+    item_group?: string;
+    brand?: string;
+    has_serial_no?: 0 | 1;
+    has_batch_no?: 0 | 1;
+    is_stock_item?: 0 | 1;
+    disabled?: 0 | 1;
+    // Standard Frappe fields
+    owner?: string;
+    creation?: string;
+    modified?: string;
+    modified_by?: string;
+    docstatus?: 0 | 1 | 2;
+  }
+  
+  export interface ItemCreateRequest {
+    item_code: string;
+    item_name: string;
+    stock_uom: string;
+    description?: string;
+    item_group?: string;
+    brand?: string;
+    has_serial_no?: 0 | 1;
+    has_batch_no?: 0 | 1;
+    is_stock_item?: 0 | 1;
+  }
+  
+  export interface ItemUpdateRequest extends Partial<ItemCreateRequest> {
+    name: string;
+  }
