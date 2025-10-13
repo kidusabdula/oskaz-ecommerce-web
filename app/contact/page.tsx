17313353a8fa48bbefc6b624afd8abaedee86332
cAlEb <101443174+caleb-cactus@users.noreply.github.com>
2025-10-13 07:52:32 +0300

    google map touch ups

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -420,7 +420,7 @@ export default function ContactPage() {
                   {/* Embedded Google Map */}
                   <div className="relative h-64 rounded-lg overflow-hidden border">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.755238724238!2d38.75794977500731!3d9.0222992910708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85e2f5c5c5c5%3A0x3e3e3e3e3e3e3e3e!2sAddis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2set!4v1234567890"
+                      src="https://www.google.com/maps?q=9.001787774540169,38.76919929544805&hl=en&z=15&output=embed"
                       width="100%"
                       height="100%"
                       style={{ border: 0 }}
